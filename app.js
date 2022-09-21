341458f8fcd2f857c41305941a6f231a3291a2f0
Vyacheslav Mityagin <mityaginvs@yandex.ru>
2022-09-21 23:31:47 +0300

    fix: очередная попытка с cors

--- app.js
+++ app.js
@@ -3,12 +3,13 @@ const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
 const { errors } = require('celebrate');
-const cors = require('./middlewares/cors');
+const cors = require('cors');
 const routes = require('./routes/routes');
 const { errorsCheck } = require('./middlewares/errors');
 const { errorLogger, requestLogger } = require('./middlewares/logger');
 
 const app = express();
+app.use(cors());
 
 app.use(cookieParser());
 app.use(bodyParser.json());
@@ -18,7 +19,6 @@ mongoose.connect('mongodb://localhost:27017/mestodb', {
   useNewUrlParser: true, useUnifiedTopology: true,
 });
 
-app.use(cors);
 app.use(requestLogger);
 app.use(routes);
 app.use(errorLogger);
