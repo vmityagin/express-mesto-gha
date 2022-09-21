b66766dfca33478a8271d799cfd895732dab24d4
Vyacheslav Mityagin <mityaginvs@yandex.ru>
2022-09-21 22:53:41 +0300

    fix: очередная попытка с cors

--- server.js
+++ server.js
@@ -1,8 +1,12 @@
 const cors = require('cors');
 const app = require('./app');
 
+const corsOptions = {
+  origin: 'http://localhost:3000',
+};
+
 const { PORT = 3000 } = process.env;
-app.use(cors());
+app.use(cors(corsOptions));
 
 app.listen(PORT, () => {
   console.log(`${PORT}`);
